48b938045482f74fa88dfca8a362d847738a083e
Songyi Son <songyi@Songyis-MBP.lan>
2024-12-01 10:25:51 -0500

    background color fixed

--- src/app/saved/page.tsx
+++ src/app/saved/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
    
 
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen" style={{ backgroundColor: "#FFFFE6"}}>
       {<NewHeader onLogOut={handleLogOut} />}
 
       <BackArrow/>
